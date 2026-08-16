d5e8c9f9a64d6b2dc6f49a1e88983ab4537365cd
jcarloscsva-eng <jcarloscsva@gmail.com>
2026-08-16 21:22:18 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@ const CONFIG = {
   GOOGLE_CLIENT_ID: '726662767018-eh76hipq9mej13jv6tm2en2jm5ompaf6.apps.googleusercontent.com',
 
   // URL de despliegue del Apps Script (Implementar > Nueva implementación > Aplicación web)
-  API_URL: 'https://script.google.com/macros/s/TU_DEPLOYMENT_ID/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbxVWDda80JrnqTkrlfsdgWeMvEPZ0GX1hss1EV-eEovT0HLV9GeyOvJMNhGDI9ihWddqg/exec',
 
   // Opcional: solo necesario si activas notificaciones push (ver SETUP.md, paso Firebase)
   FIREBASE: {
